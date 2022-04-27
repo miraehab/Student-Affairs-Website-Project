68956ce8898ad38ea800e4ae3f01c3662e025e28
MonicaSaeed <monicasaeed12@gmail.com>
2022-04-27 23:22:26 +0200

    js

---
+++ JS/validatedStudentInfo.js
@@ -0,0 +1,52 @@
+function validateForm() {
+    let n = document.getElementById("NAME").value;
+    if (n ==="s"){
+       //Please enter your full name (first & last name).
+       document.getElementById("nn").innerHTML = 55;
+    }
+
+/*
+    let i = document.getElementById("IDs").value;
+    if (){
+      
+    }
+
+
+    let g = document.getElementById("GPA").value;
+    if (i<0 || i > 4){
+      
+    }
+
+    let e = document.getElementById("EMAIL").value;
+    if(){
+
+    }
+
+    let l = document.getElementById("LEVEL").value;
+    if(l != 1 || l != 2 || l != 3 || l != 4){
+
+    }
+
+
+    let d = document.getElementById("DEPARTMENT").value;
+    if(){
+
+    }
+
+    let b = document.getElementById("date-button").value;
+    if(){
+
+    }
+
+    let g = document.getElementById("GENDER").value;
+    if(){
+
+    }
+
+    let P = document.getElementById("PHONE").value;
+    if(){
+
+    }
+
+*/
+}
