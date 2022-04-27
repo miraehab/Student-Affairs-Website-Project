322be069243f8d8156c2112b7ac40c13792c7961
Michael <mishoopop6@gmail.com>
2022-04-28 00:28:48 +0200

    fix page links

--- JS/validatedStudentInfo.js
+++ JS/validatedStudentInfo.js
@@ -1,57 +1,16 @@
+const name = document.getElementById("NAME");
+const id = document.getElementById("IDs");
+const gpa = document.getElementById("GPA");
+const email = document.getElementById("EMAIL");
+const level = document.getElementById("LEVEL");
+const department = document.getElementById("DEPARTMENT");
+const date = document.getElementById("date-button");
+const gender = document.getElementById("GENDER");
+const phone = document.getElementById("PHONE");
+const form = document.getElementById("form");
+
 function validateForm() {
-    let n = document.getElementById("NAME").value;
-    if (n ==="s"){
-       //Please enter your full name (first & last name).
-       document.getElementById("nn").innerHTML = 55;
-    }
-
-
-
-    /*00*/
-    /** */
-/*
-    let i = document.getElementById("IDs").value;
-    if (){
-      
-    }
-
-
-    let g = document.getElementById("GPA").value;
-    if (i<0 || i > 4){
-      
-    }
-
-    let e = document.getElementById("EMAIL").value;
-    if(){
-
-    }
-
-    let l = document.getElementById("LEVEL").value;
-    if(l != 1 || l != 2 || l != 3 || l != 4){
-
-    }
-
-
-    let d = document.getElementById("DEPARTMENT").value;
-    if(){
-
-    }
-
-    let b = document.getElementById("date-button").value;
-    if(){
-
-    }
-
-    let g = document.getElementById("GENDER").value;
-    if(){
-
-    }
-
-    let P = document.getElementById("PHONE").value;
-    if(){
-
-    }
-
-*/
+    
+   
 }
 
---
+++ js/validatedepart.js
@@ -0,0 +1,7 @@
+const level = '2';
+function validatedepart(){
+    if(level=='1'||level=='2'){
+        console.log("dddd");
+        window.location.href='./erorr.html';
+    }
+}
