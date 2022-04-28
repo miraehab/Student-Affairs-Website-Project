fd4358bebc6f45df1a6dbc17c8e5cd69a84141af
Michael <mishoopop6@gmail.com>
2022-04-29 00:53:12 +0200

    .

--- JS/validatedStudentInfo.js
+++ JS/validatedStudentInfo.js
@@ -1,16 +1,19 @@
-const name = document.getElementById("NAME");
-const id = document.getElementById("IDs");
-const gpa = document.getElementById("GPA");
-const email = document.getElementById("EMAIL");
-const level = document.getElementById("LEVEL");
-const department = document.getElementById("DEPARTMENT");
-const date = document.getElementById("date-button");
-const gender = document.getElementById("GENDER");
-const phone = document.getElementById("PHONE");
-const form = document.getElementById("form");
-
 function validateForm() {
-    
+    const name = document.getElementById("NAME");
+    const id = document.getElementById("IDs");
+    const gpa = document.getElementById("GPA");
+    const email = document.getElementById("EMAIL");
+    const level = document.getElementById("LEVEL");
+    const department = document.getElementById("DEPARTMENT");
+    const date = document.getElementById("date-button");
+    const gender = document.getElementById("GENDER");
+    const phone = document.getElementById("PHONE");
+    const form = document.getElementById("form");
+    console.log(name.value);
+    if(!(/^[A-Za-z\s]+$/.test(name.value))){
+        document.getElementById("nn").innerHTML = "Please enter your full name (firstname & lastname)";
+    }
+      
    
 }
 
