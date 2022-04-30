876f22f3d2497c2fca7359472fe52b7460eceb7f
Michael <mishoopop6@gmail.com>
2022-04-30 05:32:27 +0200

    make form validation functioning

--- JS/validatedStudentInfo.js
+++ JS/validatedStudentInfo.js
@@ -1,16 +1,9 @@
 function calcAge(){
-    var userinput = document.getElementById("date-button").value;
-    var dob = new Date(userinput);  
-      //calculate month difference from current date in time  
-      var month_diff = Date.now() - dob.getTime();  
-      
-      //convert the calculated difference in date format  
+    var date = document.getElementById("date-button").value;
+    var dt = new Date(date);   
+      var month_diff = Date.now() - dt.getTime();  
       var age_dt = new Date(month_diff);   
-        
-      //extract year from date      
       var year = age_dt.getUTCFullYear();  
-        
-      //now calculate the age of the user  
       var age = Math.abs(year - 1970);  
       return age;  
 }
@@ -28,36 +21,65 @@ function validateForm() {
     var select2 = document.getElementById("DEPARTMENT");
     var department = select2.options[select2.selectedIndex].value;
 
-
     var regName = /^[a-zA-Z]+ [a-zA-Z]+$/;
-    if(!regName.test(name)){
+    if(name == null || name == ""){
+    }else if(!regName.test(name)){
         document.getElementById("Pname").innerHTML = "Please enter your full name (firstname & lastname)";
+        event.preventDefault();
+    }else{
+        document.getElementById("Pname").innerHTML = "";
     }
 
-    if ( !isNaN(Sid)) {
+    if(Sid == null || Sid == ""){
+
+    }else if ( isNaN(Sid)) {
         document.getElementById("Pid").innerHTML = "ID must be all numbers";
+        event.preventDefault();
+    }else{
+        document.getElementById("Pid").innerHTML = "";
     }
 
-    if(gpa < '0' || gpa > '4'){
+    if(gpa == null || gpa == ""){
+
+    }else if(gpa < '0' || gpa > '4'){
         document.getElementById("Pgpa").innerHTML = "Invalid gpa (please enter gpa from 0 to 4)";
+        event.preventDefault();
+    }else{
+        document.getElementById("Pgpa").innerHTML = "";
     }
 
     var regEmail=/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
-    if(!(regEmail.test(email))){
+    if(email == null || email == ""){
+
+    }else if(!(regEmail.test(email))){
         document.getElementById("Pemail").innerHTML = "You have entered an invalid email address!";
+        event.preventDefault();
+    }else{
+        document.getElementById("Pemail").innerHTML = "";
     }
    
-   if( level == 0 || level== 1 && department != 0) {
+   if( (level == 0 || level== 1) && department != 0) {
         document.getElementById("Pdepart").innerHTML = "Check your level and department, if you are in level 1 or 2 choose department 'General'";
+        event.preventDefault();
+    }else{
+        document.getElementById("Pdepart").innerHTML = "";
     }
     
     let age = calcAge();
     if(age < 16){
         document.getElementById("Pbirth").innerHTML = "Age must be more than 16";
+        event.preventDefault();
+    }else{
+        document.getElementById("Pbirth").innerHTML = "";
     }
    
-   if(phone.length < 16){
+    if(phone == null || phone == ""){
+
+    }else if(phone.length != 11 || isNaN(phone)){
         document.getElementById("Pphone").innerHTML = "Invalid phone number";
+        event.preventDefault();
+    }else{
+        document.getElementById("Pphone").innerHTML = "";
     }
   
 }
